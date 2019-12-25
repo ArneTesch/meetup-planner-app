537201741f2c204473659a0df8857f2a7a0b9aa7
Arne Tesch <arnetesch@gmail.com>
2019-12-25 15:33:30 +0100

    Authorization update

--- App.tsx
+++ App.tsx
@@ -23,21 +23,21 @@ export type RootParamList = {
 };
 const httpLink = new HttpLink({ uri: "http://localhost:8000/graphql" });
 
-const client = new ApolloClient({
-  uri: "http://192.168.10.143:8000/graphql"
-});
-
 // const client = new ApolloClient({
-//   uri: "http://192.168.10.143:8000/graphql",
-//   request: async operation => {
-//     const token = await AsyncStorage.getItem("auth_token");
-//     operation.setContext({
-//       headers: {
-//         Authorization: token && `Bearer ${token}`
-//       }
-//     });
-//   }
+//   uri: "http://192.168.10.143:8000/graphql"
 // });
+
+const client = new ApolloClient({
+  uri: "http://192.168.10.143:8000/graphql",
+  request: async operation => {
+    const token = await AsyncStorage.getItem("auth_token");
+    operation.setContext({
+      headers: {
+        Authorization: token && `Bearer ${token}`
+      }
+    });
+  }
+});
 
 const Stack = createStackNavigator<RootParamList>();
 
